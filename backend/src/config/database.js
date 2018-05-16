44ef99445785090e7f5ee99750b0eb18885bdcfc
EarthW0rm <marco.a.faustino@gmail.com>
2018-05-15 22:46:30 -0300

    Incluindo a estrutura basica de conexao com o mongo

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
-module.exports = mongoose.connect('mongodb://')
+module.exports = mongoose.connect('mongodb://reactBack:senha123@localhost:27017');
