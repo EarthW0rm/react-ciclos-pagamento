86149fb26b2ef77e13a8795652b9cf0f46731daa
EarthW0rm <marco.a.faustino@gmail.com>
2018-05-18 00:08:59 -0300

    Componemte menuTree e menuItem, com um menu basico

--- frontend/src/common/blocks/components/menu.jsx
+++ frontend/src/common/blocks/components/menu.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import MenuItem from './menuitem';
+import MenuTree from './menutree';
 
 export default props => (
     <ul className="sidebar-menu">
         <li className="header">MAIN NAVIGATION</li>
+        <MenuItem path="#" label="Dashboard" icon="dashboard" />
+        <MenuTree label="Cadastro" icon="edit">
+            <MenuItem path="#billingCycle" label="Ciclos de Pagamentos" icon="usd" />
+        </MenuTree>
     </ul>
 );
