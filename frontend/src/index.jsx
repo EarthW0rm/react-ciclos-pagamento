d8f58ab7a94ff20c32e59f20f132df5a509f5ef5
EarthW0rm <marco.a.faustino@gmail.com>
2018-05-17 23:26:10 -0300

    Criacao do app, integrando com a index com um simple h1

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,3 +1,8 @@
 import './dependency/admin-lte';
-
 import './sass/main-style';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './main/app';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    render(){
+        return(
+            <div className="wrapper">
+                <h1>Say hello to my little friend</h1>
+            </div>
+        );
+    }
+
+}
+
