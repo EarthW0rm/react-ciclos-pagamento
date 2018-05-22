714e245068e671bcf742ad95e0ebfd9b001af4e1
EarthW0rm <marco.a.faustino@gmail.com>
2018-05-21 22:34:52 -0300

    Incluindo os valuebox no dashboard

--- frontend/src/common/layout/grid.jsx
+++ frontend/src/common/layout/grid.jsx
@@ -5,14 +5,16 @@ export default class Grid extends Component {
         const cols = numbers ? numbers.split(' ') : [];
         
         let classes = '';
-        if(cols[0]) classes += `col-xs-${cols[0]}`;
-        if(cols[1]) classes += `col-sm-${cols[1]}`;
-        if(cols[2]) classes += `col-md-${cols[2]}`;
-        if(cols[3]) classes += `col-lg-${cols[3]}`;
+        if(cols[0]) classes += `col-xs-${cols[0]} `;
+        if(cols[1]) classes += `col-sm-${cols[1]} `;
+        if(cols[2]) classes += `col-md-${cols[2]} `;
+        if(cols[3]) classes += `col-lg-${cols[3]} `;
+
+        return classes;
     }
 
     render(){
-        const classes = this.toCssClasses(this.props.cols || '12') + (this.props.className ? ' ' + this.props.className : '');
+        const classes = this.toCssClasses(this.props.cols || '12') + (this.props.className || '');
 
         return (
             <div className={classes}>
--- frontend/src/main/dashboard/dashboard.jsx
+++ frontend/src/main/dashboard/dashboard.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import ContentHeader from '../../common/blocks/components/contentheader';
 import Content from '../../common/blocks/components/content';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import ValueBox from '../../common/widget/valuebox';
 
 class Dashboard extends Component {
     render() { 
@@ -10,11 +11,15 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title="Dashboard" small="Painel de Controle">
                 <ol className="breadcrumb">
-                    <li><i className="fa fa-dashboard"></i> Dashboard</li>
+                    <li><i className="fa fa-dashboard" /> Dashboard</li>
                 </ol>
                 </ContentHeader>
                 <Content>
-                    <h1>Dashboard, say hello to my little friend</h1>
+                    <div className="row">
+                        <ValueBox cols="12 6 4 4" color="green" icon="plus-square" text="Total de crédito" value="20,00"/>
+                        <ValueBox cols="12 6 4 4" color="red" icon="minus-square" text="Total de dédito" value="10,00"/>
+                        <ValueBox cols="12 6 4 4" color="aqua" icon="signal" text="Total" value="10,00"/>
+                    </div>
                 </Content>
             </div> 
         )
